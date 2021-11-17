2120573d4e633fc4f172302836b3a9ef769de24a
Opilane TTHK <opilane@tthk.ee>
2021-11-17 13:35:30 +0200

    Add project files.

---
+++ Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace korsunovwebform
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2.ActiveForm.Hide(); // При нажатии любой клавиши первая форма скрывается, а вторая появляется
+            Form1 MyForm1 = new Form1();
+            MyForm1.ShowDialog();
+            Close();
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace korsunovwebform
+{
+    class Triangle
+    {
+        public double a; //первая сторона
+        public double b; //вторая сторона
+        public double c; //третья сторона
+        public double hb; //высота привязанная к стороне b
+        public Triangle(double A, double B, double C) // Конструктор
+        {
+            a = A; // создаём с заданными длинами сторон согласно заданию
+            b = B;
+            c = C;
+        }
+        public Triangle() // Конструктор, создаст треугольник без указания параметров треугольника
+        {
+        }
+
+        public string outputA() // выводим строку a, данный метод возвращает строковое значени
+        {
+            return Convert.ToString(a);
+        }
+        public string outputB() // выводим сторону b
+        {
+            return Convert.ToString(b);
+        }
+        public string outputC() // выводим сторону c
+        {
+            return Convert.ToString(c);
+        }
+        public double Perimeter() // сумма всех сторон типа
+        {
+            double p = 0;
+            p = a + b + c; // вычисление...
+            return p; // возврат
+        }
+        public double Surface() // аналогично периметру..
+        {
+            double s = 0;
+            double p = 0;
+            p = (a + b + c) / 2;
+            s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return s;
+        }
+        public double GetSetA // свойство позволяющее установить либо изменить значение стороны a
+        {
+            get // устанавливаем
+            { return a; }
+            set // меняем
+            { a = value; }
+        }
+        public double GetSetB // свойство позволяющее установить либо изменить значение стороны b
+        {
+            get
+            { return b; }
+            set
+            { b = value; }
+        }
+        public double GetSetC // свойство позволяющее установить либо изменить значение стороны c
+        {
+            get
+            { return c; }
+            set
+            { c = value; }
+        }
+        public bool ExistTriangle // свойство позволяющее установить, существует ли треугольник с заданными сторонами
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b))//сумма 2 сторон должна быть больше третьей
+                return false;
+                else return true;
+            }
+        }
+        public string TriangleType // свойство позволяющее установить, существует ли треугольник с заданными сторонами
+        {
+            get
+            {
+                if (a == b && a == c && b == c)
+                    return "равносторонний";
+                else if (a == b || a == c || b == c)
+                    return "равнобедренный";
+                else return "разносторонний";
+            }
+        }
+    }
+}
